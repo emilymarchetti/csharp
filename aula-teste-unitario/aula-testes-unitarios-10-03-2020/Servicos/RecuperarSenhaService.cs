811346f89c97d451d975d81aa6a72d8d4debe4b3
Emily Marchetti <emilymarchetti@outlook.com>
2020-03-11 19:53:54 -0300

    Aula 10/03/20

---
+++ aula-teste-unitario/aula-testes-unitarios-10-03-2020/Servicos/RecuperarSenhaService.cs
@@ -0,0 +1,48 @@
+﻿using aula_testes_unitarios_10_03_2020.Entidades;
+using aula_testes_unitarios_10_03_2020.Interfaces;
+using aula_testes_unitarios_10_03_2020.Repositorio;
+using System;
+using System.Linq;
+
+namespace aula_testes_unitarios_10_03_2020.Servicos
+{
+    internal class RecuperarSenhaService : ISubitemMenu
+    {
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public RecuperarSenhaService(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public void Executar()
+        {
+            Console.WriteLine("Informe seu usuário: ");
+            var nomeUsuario = Console.ReadLine();
+            var usuario = _usuarioRepositorio.GetAll().FirstOrDefault(x => x.Nome.Equals(nomeUsuario));
+
+            if (usuario == null)
+            {
+                Console.WriteLine("Usuário inválido.");
+                return;
+            }
+
+            Console.WriteLine("Informe a nova senha: ");
+            var senhaUsuario = Console.ReadLine();
+
+            Console.WriteLine("Informe a nova senha novamente: ");
+            var senhaUsuarioDois = Console.ReadLine();
+
+            if (senhaUsuario != senhaUsuarioDois)
+            {
+                Console.WriteLine("Senhas não conferem.");
+                return;
+            }
+                
+            usuario.SetSenha(senhaUsuario);
+            _usuarioRepositorio.Update(usuario);
+
+            Console.WriteLine("Senha alterada com sucesso!");
+        }
+    }
+}
---
+++ aula-teste-unitario/aula-testes-unitarios-10-03-2020/Servicos/UsuarioService.cs
@@ -0,0 +1,29 @@
+﻿using aula_testes_unitarios_10_03_2020.Entidades;
+using aula_testes_unitarios_10_03_2020.Interfaces;
+using aula_testes_unitarios_10_03_2020.Repositorio;
+using System;
+
+namespace aula_testes_unitarios_10_03_2020.Servicos
+{
+    internal class UsuarioService : ISubitemMenu
+    {
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioService(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public void Executar()
+        {
+            Console.WriteLine("Informe seu usuário: ");
+            var nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Informe sua senha: ");
+            var senhaUsuario = Console.ReadLine();
+
+            var usuario = new Usuario(nomeUsuario, senhaUsuario);
+            _usuarioRepositorio.Add(usuario);
+        }
+    }
+}
